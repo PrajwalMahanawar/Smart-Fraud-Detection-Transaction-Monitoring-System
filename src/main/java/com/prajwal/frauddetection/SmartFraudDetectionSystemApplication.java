1fd1cd4ecb5007a83e645ff3ea9952ee42b93d07
Prajwal Mahanawar <p.s.mahanawar2002@gmail.com>
2026-05-17 22:39:24 +0100

    push to repo

---
+++ src/main/java/com/prajwal/frauddetection/SmartFraudDetectionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.prajwal.frauddetection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFraudDetectionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartFraudDetectionSystemApplication.class, args);
+	}
+
+}
